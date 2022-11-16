9b15c74ead640fa6fa7a373e906eb19035d00600
DarkSoul14789 <shubhrajivshah@gmail.com>
2022-11-17 00:50:26 +0530

    added service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,7 +1,5 @@
 package com.example.demo.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+  private final StudentService studentService;
+
+  public StudentController(StudentService studentService) {
+    this.studentService = studentService;
+  }
+
   @GetMapping
 	public List<Student> getStudents() {
-		return List.of(
-			new Student(
-				1L,
-				"Alice",
-				"Alice@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-			);
+		return studentService.getStudents();
 	}
 }
