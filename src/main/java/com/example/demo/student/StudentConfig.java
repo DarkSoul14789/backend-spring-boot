590fa0766aaf0aac86210ed32e24d6262940052e
DarkSoul14789 <shubhrajivshah@gmail.com>
2022-11-29 00:51:17 +0530

    configured jpa repository and student table

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,38 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static java.time.Month.*; 
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StudentConfig {
+
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository){
+    return args -> {
+      Student mariam = new Student(
+        1L,
+        "Mariam",
+        "Mariam@gmail.com",
+        LocalDate.of(2000,JANUARY,5),
+        21
+      );
+
+      Student alex = new Student(
+        "Alex",
+        "Alex@gmail.com",
+        LocalDate.of(2004,JANUARY,7),
+        21
+      );
+
+      repository.saveAll(
+        List.of(mariam, alex)
+      );
+    };
+  }
+}
